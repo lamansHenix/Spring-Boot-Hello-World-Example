3a11af304cdd770fe5bd017fd312c3bb80f4a768
lamansHenix <54840385+lamansHenix@users.noreply.github.com>
2020-09-22 10:02:19 +0200

    Update HomeController.java

--- src/main/java/com/reytech/demo/controller/HomeController.java
+++ src/main/java/com/reytech/demo/controller/HomeController.java
@@ -14,7 +14,7 @@ public class HomeController {
 
     @GetMapping("/message")
     public String message(Model model) {
-        model.addAttribute("message", "This is a custom message test changement ");
+        model.addAttribute("message", "This is a custom message test changement 2 ");
         return "message";
     }
 }
